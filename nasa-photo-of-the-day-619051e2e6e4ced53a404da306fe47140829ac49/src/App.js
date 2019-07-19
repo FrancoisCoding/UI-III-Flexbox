75e06c7c821babf46e8a903478153f2708b0ff93
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-07-18 20:37:47 -0400

    Dynamic search added and routing fixed

---
+++ nasa-photo-of-the-day-619051e2e6e4ced53a404da306fe47140829ac49/src/App.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import NASAGrid from "../src/Components/NASAGrid";
+function App() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    function dayone() {
+      return axios.get(
+        "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-07-11"
+      );
+    }
+
+    function daytwo() {
+      return axios.get(
+        "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-07-08"
+      );
+    }
+
+    function daythree() {
+      return axios.get(
+        "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-07-09"
+      );
+    }
+    console.log("pie");
+    axios
+      .all([dayone(), daytwo(), daythree()])
+      .then(res => {
+        console.log("heyjune", res);
+        setData(res);
+      })
+      .catch(err => console.log(err));
+  }, []);
+  console.log("state", data);
+
+  return (
+    <div className="App">
+      <h1>NASA Photo of the Day</h1>
+      <section>
+        <NASAGrid nasaData={data} />
+      </section>
+    </div>
+  );
+}
+
+export default App;
